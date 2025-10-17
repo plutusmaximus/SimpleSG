05d23509668d45f5d1ccaa7b06e8391ed9161cf2
plutusmaximus <2399665+plutusmaximus@users.noreply.github.com>
2025-10-16 20:28:13 -0700

    Removed unneeded SDL resource for SDL_Window

--- src/SdlResource.cpp
+++ src/SdlResource.cpp
@@ -3,12 +3,6 @@
 #include <SDL3/SDL_gpu.h>
 
 template<>
-SdlResource<SDL_Window>::~SdlResource()
-{
-    if (m_Resource) { SDL_DestroyWindow(m_Resource); }
-}
-
-template<>
 SdlResource<SDL_GPUDevice>::~SdlResource()
 {
     if (m_Resource) { SDL_DestroyGPUDevice(m_Resource); }
