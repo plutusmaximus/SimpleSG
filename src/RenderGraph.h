bcff4c73b06b789b8cc0093eabdabb0b4eb291b3
plutusmaximus <2399665+plutusmaximus@users.noreply.github.com>
2025-11-03 21:57:34 -0800

    Coed cleanup

--- src/RenderGraph.h
+++ src/RenderGraph.h
@@ -17,7 +17,7 @@ public:
 
     virtual void Add(const Mat44f& viewTransform, RefPtr<ModelNode> model) = 0;
 
-    virtual Result<void> Render(const Mat44f& view, const Mat44f& projection) = 0;
+    virtual Result<void> Render(const Mat44f& camera, const Mat44f& projection) = 0;
 
     virtual void Reset() = 0;
 
