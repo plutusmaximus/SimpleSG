d583bcfec0ace39f26d8a0173c833c81c760d0ff
plutusmaximus <2399665+plutusmaximus@users.noreply.github.com>
2026-02-12 19:21:31 -0800

    Implement move semantics and iterators for intrusive linked list.  Added more tests for inlist.

--- src/inlist.h
+++ src/inlist.h
@@ -1,13 +1,15 @@
 #pragma once
 
+#include <cstddef>
+#include <iterator>
+
 /// @brief Node for an intrusive linked list
 template<typename T>
 class inlist_node
 {
 public:
-
-    T* Next{nullptr};
-    T* Prev{nullptr};
+    T* Next{ nullptr };
+    T* Prev{ nullptr };
 };
 
 /// @brief Intrusive linked list
@@ -17,11 +19,154 @@ template<typename T, inlist_node<T> T::* NodeMember>
 class inlist
 {
 public:
+    inlist() = default;
+    inlist(const inlist&) = delete;
+    inlist& operator=(const inlist&) = delete;
+    inlist(inlist&& other) noexcept
+    {
+        m_Head = other.m_Head;
+        m_Tail = other.m_Tail;
+        m_Size = other.m_Size;
+
+        other.m_Head = nullptr;
+        other.m_Tail = nullptr;
+        other.m_Size = 0;
+    }
+    inlist& operator=(inlist&& other) noexcept
+    {
+        if(this != &other)
+        {
+            m_Head = other.m_Head;
+            m_Tail = other.m_Tail;
+            m_Size = other.m_Size;
+
+            other.m_Head = nullptr;
+            other.m_Tail = nullptr;
+            other.m_Size = 0;
+        }
+        return *this;
+    }
+
+    class iterator
+    {
+    public:
+        using iterator_category = std::bidirectional_iterator_tag;
+        using value_type = T;
+        using difference_type = std::ptrdiff_t;
+        using pointer = T*;
+        using reference = T&;
+
+        iterator() = default;
+        explicit iterator(T* node)
+            : m_Node(node)
+        {
+        }
+
+        reference operator*() const { return *m_Node; }
+        pointer operator->() const { return m_Node; }
+
+        iterator& operator++()
+        {
+            eassert(m_Node != nullptr && "Cannot increment end iterator");
+            m_Node = m_Node ? (m_Node->*NodeMember).Next : nullptr;
+            return *this;
+        }
+
+        iterator operator++(int)
+        {
+            iterator tmp(*this);
+            ++(*this);
+            return tmp;
+        }
+
+        iterator& operator--()
+        {
+            eassert(m_Node != nullptr && "Cannot decrement end iterator");
+            m_Node = m_Node ? (m_Node->*NodeMember).Prev : nullptr;
+            return *this;
+        }
+
+        iterator operator--(int)
+        {
+            iterator tmp(*this);
+            --(*this);
+            return tmp;
+        }
+
+        bool operator==(const iterator& other) const { return m_Node == other.m_Node; }
+        bool operator!=(const iterator& other) const { return m_Node != other.m_Node; }
+
+    private:
+        friend class inlist;
+        T* node() const { return m_Node; }
+        T* m_Node{ nullptr };
+    };
+
+    class const_iterator
+    {
+    public:
+        using iterator_category = std::bidirectional_iterator_tag;
+        using value_type = const T;
+        using difference_type = std::ptrdiff_t;
+        using pointer = const T*;
+        using reference = const T&;
+
+        const_iterator() = default;
+        explicit const_iterator(const T* node)
+            : m_Node(node)
+        {
+        }
+        const_iterator(const iterator& it)
+            : m_Node(it.operator->())
+        {
+        }
+
+        reference operator*() const { return *m_Node; }
+        pointer operator->() const { return m_Node; }
+
+        const_iterator& operator++()
+        {
+            eassert(m_Node != nullptr && "Cannot increment end iterator");
+            m_Node = m_Node ? (const_cast<T*>(m_Node)->*NodeMember).Next : nullptr;
+            return *this;
+        }
+
+        const_iterator operator++(int)
+        {
+            const_iterator tmp(*this);
+            ++(*this);
+            return tmp;
+        }
+
+        const_iterator& operator--()
+        {
+            eassert(m_Node != nullptr && "Cannot decrement end iterator");
+            m_Node = m_Node ? (const_cast<T*>(m_Node)->*NodeMember).Prev : nullptr;
+            return *this;
+        }
+
+        const_iterator operator--(int)
+        {
+            const_iterator tmp(*this);
+            --(*this);
+            return tmp;
+        }
+
+        bool operator==(const const_iterator& other) const { return m_Node == other.m_Node; }
+        bool operator!=(const const_iterator& other) const { return m_Node != other.m_Node; }
+
+    private:
+        friend class inlist;
+        const T* node() const { return m_Node; }
+        const T* m_Node{ nullptr };
+    };
 
     void push_back(T* node)
     {
+        eassert(node != nullptr && "Node cannot be null");
         auto& listNode = node->*NodeMember;
-        eassert(listNode.Next == nullptr && listNode.Prev == nullptr && "Node is already in a list");
+        eassert(
+            listNode.Next == nullptr && listNode.Prev == nullptr && "Node is already in a list");
 
         listNode.Next = nullptr;
         listNode.Prev = m_Tail;
@@ -37,12 +182,16 @@ public:
         {
             m_Head = node;
         }
+
+        ++m_Size;
     }
 
     void push_front(T* node)
     {
+        eassert(node != nullptr && "Node cannot be null");
         auto& listNode = node->*NodeMember;
-        eassert(listNode.Next == nullptr && listNode.Prev == nullptr && "Node is already in a list");
+        eassert(
+            listNode.Next == nullptr && listNode.Prev == nullptr && "Node is already in a list");
 
         listNode.Next = m_Head;
         listNode.Prev = nullptr;
@@ -53,19 +202,68 @@ public:
         }
 
         m_Head = node;
+
+        if(!m_Tail)
+        {
+            m_Tail = node;
+        }
+
+        ++m_Size;
     }
 
-    T* front() const
+    T* front() const { return m_Head; }
+
+    T* back() const { return m_Tail; }
+
+    iterator erase(iterator pos)
     {
-        return m_Head;
+        T* node = pos.node();
+        eassert(node != nullptr && "Cannot erase end iterator");
+
+        auto& listNode = node->*NodeMember;
+        T* next = listNode.Next;
+        T* prev = listNode.Prev;
+
+        if(prev)
+        {
+            (prev->*NodeMember).Next = next;
+        }
+        else
+        {
+            m_Head = next;
+        }
+
+        if(next)
+        {
+            (next->*NodeMember).Prev = prev;
+        }
+        else
+        {
+            m_Tail = prev;
+        }
+
+        listNode.Next = nullptr;
+        listNode.Prev = nullptr;
+        --m_Size;
+
+        return iterator(next);
     }
 
-    T* back() const
-    {
-        return m_Tail;
-    }
+    iterator erase(const_iterator pos) { return erase(iterator(const_cast<T*>(pos.node()))); }
+
+    std::size_t size() const { return m_Size; }
+
+    bool empty() const { return m_Size == 0; }
+
+    iterator begin() { return iterator(m_Head); }
+    iterator end() { return iterator(nullptr); }
+    const_iterator begin() const { return const_iterator(m_Head); }
+    const_iterator end() const { return const_iterator(nullptr); }
+    const_iterator cbegin() const { return const_iterator(m_Head); }
+    const_iterator cend() const { return const_iterator(nullptr); }
+
 private:
-
-    T* m_Head{nullptr};
-    T* m_Tail{nullptr};
+    T* m_Head{ nullptr };
+    T* m_Tail{ nullptr };
+    std::size_t m_Size{ 0 };
 };
--- tests/inlist.test.cpp
+++ tests/inlist.test.cpp
@@ -11,21 +11,153 @@ public:
     inlist_node<Foo> ListNode2;
 };
 
-// Test default constructor
-TEST(inlist, TestAll)
+TEST(inlist, PushAndFrontBack)
+{
+    inlist<Foo, &Foo::ListNode1> list;
+    Foo foos[3];
+
+    list.push_back(&foos[0]);
+    EXPECT_EQ(list.front(), &foos[0]);
+    EXPECT_EQ(list.back(), &foos[0]);
+
+    list.push_front(&foos[1]);
+    EXPECT_EQ(list.front(), &foos[1]);
+    EXPECT_EQ(list.back(), &foos[0]);
+
+    list.push_back(&foos[2]);
+    EXPECT_EQ(list.front(), &foos[1]);
+    EXPECT_EQ(list.back(), &foos[2]);
+}
+
+TEST(inlist, MultipleListsIndependent)
 {
     inlist<Foo, &Foo::ListNode1> list1;
     inlist<Foo, &Foo::ListNode2> list2;
-    EXPECT_EQ(list1.front(), nullptr);
-    EXPECT_EQ(list2.front(), nullptr);
-
-    Foo foos[3];
+    Foo foos[2];
 
     list1.push_back(&foos[0]);
+    list2.push_back(&foos[0]);
+    list2.push_back(&foos[1]);
+
+    EXPECT_EQ(list1.size(), 1u);
+    EXPECT_EQ(list2.size(), 2u);
     EXPECT_EQ(list1.front(), &foos[0]);
-    list1.push_back(&foos[1]);
-    EXPECT_EQ(list1.front(), &foos[0]);
-    EXPECT_EQ(list1.back(), &foos[1]);
-    list1.push_front(&foos[2]);
-    EXPECT_EQ(list1.front(), &foos[2]);
+    EXPECT_EQ(list2.front(), &foos[0]);
+    EXPECT_EQ(list2.back(), &foos[1]);
+
+    list1.erase(list1.begin());
+    EXPECT_TRUE(list1.empty());
+    EXPECT_EQ(list2.size(), 2u);
+    EXPECT_EQ(list2.front(), &foos[0]);
+    EXPECT_EQ(list2.back(), &foos[1]);
+
+    auto it = list2.begin();
+    ++it; // foos[1]
+    list2.erase(it);
+    EXPECT_EQ(list2.size(), 1u);
+    EXPECT_EQ(list2.front(), &foos[0]);
+    EXPECT_EQ(list2.back(), &foos[0]);
+}
+
+TEST(inlist, SizeAndEmpty)
+{
+    inlist<Foo, &Foo::ListNode1> list;
+    Foo foos[3];
+
+    EXPECT_TRUE(list.empty());
+    EXPECT_EQ(list.size(), 0u);
+
+    list.push_back(&foos[0]);
+    EXPECT_FALSE(list.empty());
+    EXPECT_EQ(list.size(), 1u);
+
+    list.push_front(&foos[1]);
+    EXPECT_EQ(list.size(), 2u);
+
+    list.push_back(&foos[2]);
+    EXPECT_EQ(list.size(), 3u);
+}
+
+TEST(inlist, IterationOrder)
+{
+    inlist<Foo, &Foo::ListNode1> list;
+    Foo foos[3];
+
+    list.push_back(&foos[0]);
+    list.push_back(&foos[1]);
+    list.push_back(&foos[2]);
+
+    int index = 0;
+    for(auto it = list.begin(); it != list.end(); ++it)
+    {
+        EXPECT_EQ(&(*it), &foos[index]);
+        ++index;
+    }
+    EXPECT_EQ(index, 3);
+}
+
+TEST(inlist, ConstIteration)
+{
+    inlist<Foo, &Foo::ListNode1> list;
+    Foo foos[2];
+
+    list.push_back(&foos[0]);
+    list.push_back(&foos[1]);
+
+    const auto& clist = list;
+    auto it = clist.cbegin();
+    EXPECT_EQ(&(*it), &foos[0]);
+    ++it;
+    EXPECT_EQ(&(*it), &foos[1]);
+    ++it;
+    EXPECT_EQ(it, clist.cend());
+}
+
+TEST(inlist, EraseByIterator)
+{
+    inlist<Foo, &Foo::ListNode1> list;
+    Foo foos[4];
+
+    list.push_back(&foos[0]);
+    list.push_back(&foos[1]);
+    list.push_back(&foos[2]);
+    list.push_back(&foos[3]);
+
+    auto it = list.begin();
+    ++it; // foos[1]
+    it = list.erase(it);
+
+    EXPECT_EQ(list.size(), 3u);
+    EXPECT_EQ(list.front(), &foos[0]);
+    EXPECT_EQ(list.back(), &foos[3]);
+    EXPECT_EQ(&(*it), &foos[2]);
+
+    list.erase(list.begin());
+    EXPECT_EQ(list.size(), 2u);
+    EXPECT_EQ(list.front(), &foos[2]);
+
+    auto tailIt = list.begin();
+    ++tailIt; // foos[3]
+    tailIt = list.erase(tailIt);
+    EXPECT_EQ(list.size(), 1u);
+    EXPECT_EQ(list.back(), &foos[2]);
+    EXPECT_EQ(tailIt, list.end());
+}
+
+TEST(inlist, EraseByConstIterator)
+{
+    inlist<Foo, &Foo::ListNode1> list;
+    Foo foos[3];
+
+    list.push_back(&foos[0]);
+    list.push_back(&foos[1]);
+    list.push_back(&foos[2]);
+
+    const auto& clist = list;
+    auto it = clist.cbegin();
+    ++it; // foos[1]
+
+    auto next = list.erase(it);
+    EXPECT_EQ(list.size(), 2u);
+    EXPECT_EQ(&(*next), &foos[2]);
 }
