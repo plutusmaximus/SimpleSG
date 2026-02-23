f70ee3a1877f4dfe32bfba4d2ab782344ea671ec
plutusmaximus <2399665+plutusmaximus@users.noreply.github.com>
2026-02-23 10:54:32 -0800

    Refactor CacheKey and Material headers: remove redundant code and streamline includes

--- src/Material.h
+++ src/Material.h
@@ -1,78 +1,10 @@
 #pragma once
 
+#include "CacheKey.h"
 #include "Color.h"
-#include <string_view>
 #include <variant>
 
 class GpuTexture;
-
-/// @brief Unique key for caching resources.
-class CacheKey
-{
-    friend std::hash<CacheKey>;
-
-public:
-
-    explicit CacheKey(const char* key)
-        : m_Key(key ? key : "")
-        , m_HashCode(std::hash<std::string_view>()(key ? key : ""))
-    {
-        // CacheKey must not be empty.
-        eassert(key && key[0] != '\0');
-    }
-
-    explicit CacheKey(std::string_view key)
-        : m_Key(key)
-        , m_HashCode(std::hash<std::string_view>()(key))
-    {
-        // CacheKey must not be empty.
-        eassert(!key.empty());
-    }
-    explicit CacheKey(const imstring& key)
-        : m_Key(key)
-        , m_HashCode(std::hash<std::string_view>()(key))
-    {
-        // CacheKey must not be empty.
-        eassert(!key.empty());
-    }
-
-    bool operator==(const CacheKey& other) const
-    {
-        return m_HashCode == other.m_HashCode && m_Key == other.m_Key;
-    }
-
-    bool operator!=(const CacheKey& other) const
-    {
-        return !(*this == other);
-    }
-
-    bool operator<(const CacheKey& other) const
-    {
-        if(m_HashCode < other.m_HashCode)
-        {
-            return true;
-        }
-        else if(m_HashCode > other.m_HashCode)
-        {
-            return false;
-        }
-        else
-        {
-            return m_Key < other.m_Key;
-        }
-    }
-
-    const imstring& ToString() const
-    {
-        return m_Key;
-    }
-
-private:
-    CacheKey() = delete;
-
-    imstring m_Key;
-    size_t m_HashCode;
-};
 
 /// @brief Specification for creating a texture.
 class TextureSpec
@@ -329,16 +261,6 @@ private:
 
     /// @brief Base (albedo) texture of the material.
     GpuTexture* m_BaseTexture{ nullptr };
-};
-
-/// @brief Enable hashing of CacheKey for use in unordered containers.
-template<>
-struct std::hash<CacheKey>
-{
-    std::size_t operator()(const CacheKey& key) const noexcept
-    {
-        return key.m_HashCode;
-    }
 };
 
 /// @brief Enable hashing of MaterialId for use in unordered containers.
