cb71cce1759de16a72a3e2f2242d4dfb7e96b12b
plutusmaximus <2399665+plutusmaximus@users.noreply.github.com>
2026-04-12 17:52:59 -0700

    Moved scenekit creation out of cgltfloader into DawnSceneKit

--- src/CgltfModelLoader.h
+++ src/CgltfModelLoader.h
@@ -1,50 +1,13 @@
 #pragma once
 
-#include "Color.h"
 #include "Result.h"
-#include "VecMath.h"
 #include "SceneKit.h"
 
-#include <limits>
 #include <string>
-#include <vector>
-
-class SceneKit;
-
-using ModelIndex = uint32_t;
-
-namespace wgpu
-{
-class Device;
-}
-
-struct MaterialData
-{
-    std::string BaseTextureUri;
-    RgbaColorf Color;
-    float Metalness;
-    float Roughness;
-};
-
-struct MeshData
-{
-    uint32_t FirstIndex;
-    uint32_t IndexCount;
-    uint32_t BaseVertex;
-    MaterialIndex MaterialIndex;
-};
-
-struct TransformData
-{
-    static constexpr TransformIndex kInvalidParentIndex = std::numeric_limits<TransformIndex>::max();
-
-    Mat44f Transform;
-    TransformIndex ParentIndex{ kInvalidParentIndex };
-};
 
 class CgltfModelLoader final
 {
 public:
 
-    static Result<SceneKit*> LoadSceneKit(wgpu::Device& device, const std::string& path);
+    static Result<SceneKitSourceData> LoadSceneKit(const std::string& path);
 };
