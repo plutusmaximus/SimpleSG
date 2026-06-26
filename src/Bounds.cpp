713254f861e62511f0c817fcc9328b1f7b33c0f2
Kevin Baca <github@aidiastudios.com>
2026-06-25 20:02:32 -0700

    Added invariants to bounding box calculation to prevent invalid boxes

--- src/Bounds.cpp
+++ src/Bounds.cpp
@@ -24,6 +24,13 @@ ComputeMax(const Vec3f& p0, const Vec3f& p1)
         };
 }
 
+Vec3f ComputeCenter(const Vec3f& p0, const Vec3f& p1)
+{
+    const Vec3f min = ComputeMin(p0, p1);
+    const Vec3f max = ComputeMax(p0, p1);
+    return min + (max - min) * 0.5f;
+}
+
 Vec3f
 ComputeHalfExtents(const Vec3f& p0, const Vec3f& p1)
 {
@@ -34,11 +41,9 @@ ComputeHalfExtents(const Vec3f& p0, const Vec3f& p1)
 } // namespace
 
 BoundingBox::BoundingBox(const Vec3f& p0, const Vec3f& p1) noexcept
-    : m_Center(p0 + ((p1 - p0) * 0.5f)),
+    : m_Center(ComputeCenter(p0, p1)),
       m_HalfExtents(ComputeHalfExtents(p0, p1))
 {
-    MLG_ABORTIF(m_HalfExtents.x < 0.0f || m_HalfExtents.y < 0.0f || m_HalfExtents.z < 0.0f,
-        "Bounding box half extents must not be negative");
 }
 
 BoundingBox
@@ -58,19 +63,25 @@ operator+(const BoundingBox& a, const BoundingBox& b) noexcept // NOLINT(misc-us
 BoundingBox
 BoundingBox::FromVertices(std::span<const Vertex> vertices, std::span<const VertexIndex> indices)
 {
+    MLG_ABORTIF(vertices.empty(), "Cannot compute bounding box from empty vertex list");
+    MLG_ABORTIF(indices.empty(), "Cannot compute bounding box from empty index list");
+
     Vec3f min = vertices[indices[0]].pos;
     Vec3f max = vertices[indices[0]].pos;
 
-    for(const VertexIndex& index : indices)
+    if(indices.size() > 1)
     {
-        const Vec3f& pos = vertices[index].pos;
-        min.x = std::min(min.x, pos.x);
-        min.y = std::min(min.y, pos.y);
-        min.z = std::min(min.z, pos.z);
+        for(const VertexIndex& index : indices.subspan(1))
+        {
+            const Vec3f& pos = vertices[index].pos;
+            min.x = std::min(min.x, pos.x);
+            min.y = std::min(min.y, pos.y);
+            min.z = std::min(min.z, pos.z);
 
-        max.x = std::max(max.x, pos.x);
-        max.y = std::max(max.y, pos.y);
-        max.z = std::max(max.z, pos.z);
+            max.x = std::max(max.x, pos.x);
+            max.y = std::max(max.y, pos.y);
+            max.z = std::max(max.z, pos.z);
+        }
     }
 
     return BoundingBox(min, max);
